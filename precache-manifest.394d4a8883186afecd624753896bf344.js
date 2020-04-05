138390f83c92e9d5c16bc7f451b8ec9705ebff64
Dane David <dane@surveysparrow.com>
2020-04-06 01:49:27 +0530

    Updates

---
+++ precache-manifest.394d4a8883186afecd624753896bf344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b86e1afbf73af0b552f6b36303c0f96",
+    "url": "/movie-list/index.html"
+  },
+  {
+    "revision": "e2989529e2e92053adc5",
+    "url": "/movie-list/static/css/2.9b46dffb.chunk.css"
+  },
+  {
+    "revision": "177b2c7a3130e29db3e0",
+    "url": "/movie-list/static/css/main.9ccc9fba.chunk.css"
+  },
+  {
+    "revision": "e2989529e2e92053adc5",
+    "url": "/movie-list/static/js/2.9b71cedc.chunk.js"
+  },
+  {
+    "revision": "ab07a9a742436dc2c5f2d0004e0cebbf",
+    "url": "/movie-list/static/js/2.9b71cedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177b2c7a3130e29db3e0",
+    "url": "/movie-list/static/js/main.19a126f5.chunk.js"
+  },
+  {
+    "revision": "299b13bc10ec0cea2e3f",
+    "url": "/movie-list/static/js/runtime-main.7727dafb.js"
+  }
+]);
